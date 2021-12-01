3ac6410b8b9508ff0e452d2f20a2ea8773d7eeaf
Jakob <>
2021-12-01 20:51:42 +0100

    day 1, part 2: sliding window

--- run.py
+++ run.py
@@ -13,12 +13,12 @@ inputname = sys.argv[1]
 
 countIncreased = 0
 with open(inputname, "r") as inf:
-    previous = int(next(inf))
+    window = [int(next(inf)), int(next(inf)), int(next(inf))]
     for line in inf:
         value = int(line)
-        if value > previous:
+        if window[1] + window[2] + value > sum(window):
             countIncreased += 1
-        previous = value
+        window = window[1:] + [value]
 
     print(countIncreased)
 
